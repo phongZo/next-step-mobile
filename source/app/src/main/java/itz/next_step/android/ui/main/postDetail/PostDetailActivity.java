fdb904178bb3f98546164d59547dcee8e8e67a28
ThuyLinh09 <TieuHoLy.09>
2025-06-15 21:21:33 +0700

    fix toolbar scroll

--- source/app/src/main/java/itz/next_step/android/ui/main/postDetail/PostDetailActivity.java
+++ source/app/src/main/java/itz/next_step/android/ui/main/postDetail/PostDetailActivity.java
@@ -2,10 +2,13 @@ package itz.next_step.android.ui.main.postDetail;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.annotation.Nullable;
 import androidx.lifecycle.MutableLiveData;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
@@ -26,6 +29,13 @@ public class PostDetailActivity extends BaseActivity<ActivityPostDetailBinding, 
         viewBinding.btnBack.setOnClickListener(v-> {finish();});
         customTabLayout();
 
+        setupToolbar();
+        viewBinding.btnBackCollapsed.setOnClickListener(v-> {finish();});
+        loadData();
+
+    }
+
+    private void loadData() {
         Long postId = getIntent().getLongExtra("post_id", -1);
 
         if (postId != -1) {
@@ -38,6 +48,7 @@ public class PostDetailActivity extends BaseActivity<ActivityPostDetailBinding, 
         });
         viewModel.getPostDetail().observe(this, postDetail ->{
             viewBinding.tvJobPosition.setText(postDetail.getName());
+            viewBinding.tvJobPositionCollapsed.setText(postDetail.getName());
             viewBinding.tvCompanyName.setText(postDetail.getCompany().getName());
 
             String salaryText = (postDetail.getMinSalary() / 1000000) + " - " + (postDetail.getMaxSalary()/1000000) + " triệu";
@@ -49,10 +60,28 @@ public class PostDetailActivity extends BaseActivity<ActivityPostDetailBinding, 
             viewModel.loadLogo(postDetail.getCompany().getLogo());
 
         });
+    }
 
+    private void setupToolbar() {
+        viewBinding.appbar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShown = false;
+            int scrollRange = -1;
 
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
 
-
+                if (scrollRange + verticalOffset == 0) {
+                    viewBinding.toolbarCollapse.setVisibility(View.VISIBLE);
+                    isShown = true;
+                } else if (isShown) {
+                    viewBinding.toolbarCollapse.setVisibility(View.GONE);
+                    isShown = false;
+                }
+            }
+        });
     }
 
     private void customTabLayout() {
